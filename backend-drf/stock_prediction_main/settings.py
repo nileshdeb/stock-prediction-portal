869e739766e639be81ac64b37a9830d70ba40c9c
Nilesh Deb <debnilesh69@gmail.com>
2026-01-07 00:18:27 +0530

    registration page

--- backend-drf/stock_prediction_main/settings.py
+++ backend-drf/stock_prediction_main/settings.py
@@ -42,10 +42,13 @@ INSTALLED_APPS = [
     "rest_framework",
     "accounts",
     "api",
+    "corsheaders",
+
 
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -121,3 +124,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+
+CORS_ALLOWED_ORIGINS =[
+    'http://localhost:5173'
+]
+
