320c8d2296d8baa3cc812a4c89df4e89eedacb52
Nilesh Deb <debnilesh69@gmail.com>
2026-01-12 23:08:18 +0530

    integrading machine learning model with django & react

--- backend-drf/api/urls.py
+++ backend-drf/api/urls.py
@@ -4,12 +4,19 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
+from .views import StockPredictionAPIView
+
+
 urlpatterns = [
    path('register/', UserViews.RegisterView.as_view()),
 
    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
-   path('protected-view/', UserViews.ProjectedView.as_view())
+   path('protected-view/', UserViews.ProjectedView.as_view()),
+
+   # Prediction API
+
+   path('predict/',  StockPredictionAPIView.as_view(), name ='stock_prediction')
 
 ]
--- backend-drf/stock_prediction_main/settings.py
+++ backend-drf/stock_prediction_main/settings.py
@@ -146,3 +146,8 @@ SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=15),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
 }
+
+# Media fies congfiguration
+
+MEDIA_URL ='/media/'
+MEDIA_ROOT = BASE_DIR /'media'
