97240742b1805641e5d3802e2a0bf74ddf54f064
Nilesh Deb <debnilesh69@gmail.com>
2026-01-13 13:49:25 +0530

    Baackend update

--- backend-drf/stock_prediction_main/settings.py
+++ backend-drf/stock_prediction_main/settings.py
@@ -29,7 +29,11 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [ 
+    "localhost",
+    "127.0.0.1",
+    ".onrender.com"
+    ]
 
 
 # Application definition
@@ -128,10 +132,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
-CORS_ALLOWED_ORIGINS =[
-    'http://localhost:5173'
-]
+
+CORS_ALLOW_ALL_ORIGINS = True
+
 
 REST_FRAMEWORK = {
     
